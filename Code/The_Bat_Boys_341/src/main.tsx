d53d69f338bc84c1cbfce86918df00a60269f1a1
Cemi-am <mezimez.samy@gmail.com>
2024-09-28 22:30:09 -0400

    added strict to main

--- Code/The_Bat_Boys_341/src/main.tsx
+++ Code/The_Bat_Boys_341/src/main.tsx
@@ -1,8 +1,11 @@
-//import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './main.css'
 
 createRoot(document.getElementById('root')!).render(
-  <App />,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
