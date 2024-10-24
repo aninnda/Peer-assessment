579bbb2d58ef260fff2b96a52c1b523ae78fbb0e
dmoos42 <dmoos0404@gmail.com>
2024-10-24 02:11:48 -0400

    cleaning up db

--- .vscode/bat_boys_db.session.sql
+++ .vscode/bat_boys_db.session.sql
@@ -23,17 +23,3 @@ CREATE TABLE bat_boys_db.ratings (
 INSERT INTO bat_boys_db.ratings (id, name, team, conceptualContribution, practicalContribution, workEthic) VALUES
 (1, 'Dylan Moos', 'bat boys', 3, 4, 5),
 (2, 'Samy Mezimez', 'bat boys', 4, 3, 5);
-=======
-CREATE DATABASE bat_boys_db;
-
-USE bat_boys_db;
-
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    role VARCHAR(50) NOT NULL,
-    team VARCHAR(100)
-);
->>>>>>> 0280ccb5e602c239a447a9cabf48ed3030c2cef9
